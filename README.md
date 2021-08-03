d1ff2331d7ecb860ace061ae4ec599885a4d0cc2
Isha Sharma <isha@getfundwave.com>
2021-08-03 11:57:58 +0530

    0.0.2 lit-react-wrapper

---
+++ README.md
@@ -0,0 +1,25 @@
+### Lit React Wrapper 0.0.2
+
+Use React or Material-UI components in your lit-element web component.
+
+### Install
+
+```
+npm i lit-react-wrapper
+```
+
+### Usage
+
+```
+// Import the lit-react-wrapper to your existing web component project
+import "lit-react-wrapper";
+
+// Import the react component
+import {ProfileCard} from "my-react-project";
+
+// Props to be sent to the react component
+let props = {"name":"Isha Sharma","organization":"Fundwave"}
+
+// Render the react component inside your web component
+<lit-react-wrapper .element=${ProfileCard} .props=${props}></lit-react-wrapper>
+```
---
+++ lit-react-wrapper.js
@@ -0,0 +1,61 @@
+import { html, LitElement } from 'lit-element';
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as retargetEvents from 'react-shadow-dom-retarget-events';
+import { StylesProvider, jssPreset } from '@material-ui/styles';
+import { create } from 'jss';
+
+
+class LitReactWrapper extends LitElement {
+  
+  static get properties(){
+    return {
+      mountPoint: Object,
+      props: Object,
+      element: Object
+    }
+  }
+  
+  constructor(){
+    super();
+    this.props={};
+  }
+  
+  render() {
+    return html`
+      <div id="mountPoint"></div>
+    `;
+  }
+
+  createElement() {
+    this.reactElement = React.createElement(this.element, this.props, React.createElement('slot'));
+    return this.reactElement;
+  }
+  
+  renderElement(){
+    const jss = create({
+      ...jssPreset(),
+      insertionPoint: this.mountPoint
+    });
+    ReactDOM.render(
+      <StylesProvider jss={jss} sheetsManager={new Map()}>
+        {this.createElement()}
+      </StylesProvider>,
+      this.mountPoint
+    );
+  }
+
+  firstUpdated() {
+    this.mountPoint = this.shadowRoot.getElementById('mountPoint');
+    this.renderElement();
+    retargetEvents(this.shadowRoot);
+  }
+
+  updated(changedProperties){
+    if(changedProperties)
+      this.renderElement();
+  }
+
+}
+
+window.customElements.define('lit-react-wrapper', LitReactWrapper);
